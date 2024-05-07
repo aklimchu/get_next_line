cbc71e9be1a753a463b6bf3d2b9ce69f845bb6a9
Andrei Klimchuk <aklimchu@student.hive.fi>
2024-05-07 14:25:02 +0300

    Created header file and utils file

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aklimchu <aklimchu@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/07 14:04:18 by aklimchu          #+#    #+#             */
+/*   Updated: 2024/05/07 14:17:27 by aklimchu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+char	*get_next_line(int fd);
+
+# define BUFFER_SIZE 1024 ???
+
+#endif /* GET_NEXT_LINE_H */
